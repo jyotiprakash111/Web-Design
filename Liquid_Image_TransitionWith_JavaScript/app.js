260cac12cbff276a5166b66082f792150373ea63
Jyotiprakash Sethy <jyotiprakash526@gmail.com>
2019-08-13 17:42:59 +0530

    Liquid Image Hover Effect

---
+++ Liquid_Image_TransitionWith_JavaScript/app.js
@@ -0,0 +1,11 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: "./bw1.png",
+    image2: "./bw2.png",
+    // angle: Math.PI / 8,
+    // speedIn: 2,
+    // speedOut: 5,
+
+    displacementImage: "./diss.png"
+});
